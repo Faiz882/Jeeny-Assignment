21ab42c7e8b68b42dd113c0dd78863348d7cd2a6
Faiz882 <Faizmalik882@gmail.com>
2022-05-15 23:13:29 +0500

    add splash screen

--- app/src/main/java/com/assignment/jeeny/base/BaseActivity.kt
+++ app/src/main/java/com/assignment/jeeny/base/BaseActivity.kt
@@ -4,9 +4,4 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jeeny.R
 
-abstract class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
-    }
-}
+abstract class BaseActivity : AppCompatActivity()
---
+++ app/src/main/java/com/assignment/jeeny/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.assignment.jeeny.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.MainThread
+import androidx.lifecycle.lifecycleScope
+import com.assignment.jeeny.MainActivity
+import com.example.jeeny.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        lifecycleScope.launch {
+            delay(3000)
+            navigateToMainScreen()
+        }
+    }
+
+    @MainThread
+    fun navigateToMainScreen() = startActivity(Intent(this, MainActivity::class.java))
+}
